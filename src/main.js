35e4479db4ab5c0ec6d2c269a59ee45fa9bd3a3f
LexiosAlex <lexiosma@gmail.com>
2019-03-01 04:58:12 +0300

    Генератор случайных фильмов + шаблонизация

--- src/main.js
+++ src/main.js
@@ -36,35 +36,67 @@ const removeFilters = () => {
   });
 };
 
+
 removeFilters();
 renderFilter(filterElements);
 
-const filmExample = {
-  filmTitle: `Shrek2`,
-  rating: 10,
-  year: 2007,
-  duration: `1h 32min`,
-  genre: `fantasy`,
-  poster: `./images/posters/three-friends.jpg`,
-  description: `Фильм про болото, руке волос, жожо корма`,
-  commentsCount: 69
-};
+const films = [`Shrek2`, `The movie`, `Shrek3`, `Robocop`, `My cop`, `Warcraft`, `Breaking Bad`, `Banshi`, `Skyline`, `Loot`, `Bullets`, `Tape`, `Battle Angel`, `Napoleon`];
+const posterPhotos = [`three-friends.jpg`, `accused.jpg`, `blackmail.jpg`, `blue-blazes.jpg`, `fuga-da-new-york.jpg`, `moonrise.jpg`];
+const DescriptionSentenses = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+  `Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra.`,
+  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+  `Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus.`,
+  `In rutrum ac purus sit amet tempus.`,
+];
 
-const renderFilmCard = (data, count, area) => {
+
+const renderFilmCard = (count, area) => {
   for (let i = 0; i < count; i++) {
-    createFilmCard(data, area);
+
+    const getDescription = (times) => {
+      let emptyString = ``;
+      let stringArray = [];
+      for (let j = 0; j < times; j++) {
+        stringArray[j] = DescriptionSentenses[Math.floor(Math.random() * DescriptionSentenses.length)];
+        emptyString += stringArray[j];
+      }
+      return emptyString;
+    };
+
+    const filmExample = {
+      filmTitle: films[Math.floor(Math.random() * films.length)],
+      rating: (Math.random() * 10).toFixed(1),
+      year: new Date(Math.random() * 3600 * 1000 * 24 * 365 * 45),
+      duration: new Date(Math.random() * 3600 * 1000 * Math.floor(Math.random() * 2 + 1)),
+      genres: new Set([
+        `fantasy`,
+        `detective`,
+        `battle`,
+        `romantic`,
+      ]),
+      poster: `./images/posters/${posterPhotos[Math.floor(Math.random() * posterPhotos.length)]}`,
+      description: getDescription(Math.floor(Math.random() * 2 + 1)),
+      commentsCount: Math.floor(Math.random() * 100),
+    };
+
+    createFilmCard(filmExample, area);
   }
 };
 
 const renderExamples = () => {
   ALL_FILMS_AREA.forEach((item) => {
-    renderFilmCard(filmExample, 2, item);
+    renderFilmCard(2, item);
   });
 };
 
 renderExamples();
 
-renderFilmCard(filmExample, 5, FILMS_LIST_MAIN);
+renderFilmCard(5, FILMS_LIST_MAIN);
 
 const removeFilmCards = () => {
   let filmsArray = FILMS_LIST_MAIN.querySelectorAll(`.film-card`);
@@ -78,7 +110,7 @@ const filtersListener = () => {
     item.addEventListener(`click`, () => {
       removeFilmCards();
       let randomValue = Math.floor(Math.random() * 10);
-      renderFilmCard(filmExample, randomValue, ALL_FILMS_AREA[0]);
+      renderFilmCard(randomValue, ALL_FILMS_AREA[0]);
     });
   });
 };
